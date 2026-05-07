3c4aa62e199a82ce79ec00f541984aca33b19842
Tanbiru <tanvir12tt@gmail.com>
2026-05-07 19:07:02 +0600

    first commit

---
+++ solutions.ts
@@ -0,0 +1,24 @@
+// Solution 1
+
+function filterEvenNumbers(numbers: number[]): number[] {
+    return numbers.filter((num) => num % 2 === 0);
+}
+
+filterEvenNumbers([1, 2, 3, 4, 5, 6]);
+
+
+
+
+// Solution 2
+
+function reverseString(text: string): string {
+  let reversed = "";
+
+  for (let i = text.length - 1; i >= 0; i--) {
+    reversed += text[i];
+  }
+
+  return reversed;
+}
+
+ reverseString("typescript"); 
